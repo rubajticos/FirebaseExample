008728668bb9f819cc3f64463aa75e5a7a187135
Michal Rubajczyk <rubajticos@gmail.com>
2022-05-05 07:16:56 +0200

    Init project

---
+++ app/src/main/java/pl/rubajticos/firebaseexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package pl.rubajticos.firebaseexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
