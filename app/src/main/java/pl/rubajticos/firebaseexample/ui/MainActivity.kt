27d6ed7fb642c140707314508f7b5c5a403c0f04
Michal Rubajczyk <rubajticos@gmail.com>
2022-05-16 22:39:40 +0200

    Add bottom menu

--- app/src/main/java/pl/rubajticos/firebaseexample/ui/MainActivity.kt
+++ app/src/main/java/pl/rubajticos/firebaseexample/ui/MainActivity.kt
@@ -2,6 +2,12 @@ package pl.rubajticos.firebaseexample.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import pl.rubajticos.firebaseexample.R
 
@@ -10,5 +16,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupBottomNav(navController)
+    }
+
+
+    private fun setupBottomNav(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav?.setupWithNavController(navController)
     }
 }
